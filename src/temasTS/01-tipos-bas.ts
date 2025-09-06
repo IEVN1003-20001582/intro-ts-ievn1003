6f010e720ceaaf64f5fe4ca4246ad13a101a3278
IEVN1003-20001582 <jonatanlp0071@gmail.com>
2025-09-05 18:49:04 -0600

    segundo commit 05/09/2025

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,19 @@
+console.log("Hola mundo")
+
+//DECLARACIONESD DE VARIABLES
+var num1=23;
+let num2=32;
+const num3=67;
+
+let nombre:string;
+let num:number;
+let activo:boolean;
+
+let sum = num1+num2+num3
+console.log("la suma es: " + sum)
+
+let matricula:number|string
+matricula=21354351
+matricula='AHSGIW'
+console.log(matricula)
+
